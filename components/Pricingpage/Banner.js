f1e58b27dff01196d064708c3c7cd15509e9be2f
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-09 18:48:36 +0100

    Added pricing banner & list

---
+++ components/Pricingpage/Banner.js
@@ -0,0 +1,16 @@
+import { Heading, Box, Text } from "@chakra-ui/react"
+
+export default function PricingBanner(){
+    return(
+        <Box>
+            <Heading>
+            Browser Flawlessly. Subscribe Effortlessly. Interact Authentically.
+            </Heading>
+            <Text>
+                We've built simpler network modem for busy people. 
+                Enjoy our free plan as you get started, 
+                or trial our full toolkit priced to make browsing on social affordable.
+            </Text>
+        </Box>
+    )
+}
---
+++ components/Pricingpage/List.js
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react"
+
+export default function PricingList(){
+    return(
+        <Box 
+        backgroundImage={"url('images/pricing_b.jpg')"} 
+        backgroundSize={'cover'} 
+        backgroundPosition={'center'} 
+        backgroundRepeat={'no-repeat'}
+        width={'100%'} 
+        height={'500px'}
+        mt={'5em'}
+        >
+
+        </Box>
+    )
+}
