f72306f58618546a80d2599bd064553baadf01d3
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-10 13:56:56 +0100

    Modified modem list

--- components/Pricingpage/List.js
+++ components/Pricingpage/List.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text, HStack, Button } from "@chakra-ui/react"
+import { Box, Flex, Heading, Text, HStack, Button, Stack } from "@chakra-ui/react"
 import data from '../../lib/data.json'
 
 export default function PricingList(){
@@ -13,11 +13,17 @@ export default function PricingList(){
             height={'500px'}
             mt={'5em'}
             >
-            </Box>
-
-            {/* Pricing list */}
+        </Box>
         <Heading textAlign={'center'}>Choose your Modem</Heading>
-            <Box>
+            <Box 
+            //  backgroundImage={"url('images/pricing_b.jpg')"} 
+            //  backgroundSize={'cover'} 
+            //  backgroundPosition={'center'} 
+            //  backgroundRepeat={'no-repeat'}
+            //  width={'100%'} 
+            //  height={'500px'}
+            //  mt={'5em'}
+             >
                 <ModemList />
             </Box>
         </>
@@ -28,9 +34,9 @@ export default function PricingList(){
 export const ModemList = () => {
     return(
         <Box>
-            <Flex alignItems={'center'} justifyContent={'space-around'} flexDirection={'row'}>
+            <HStack alignItems={'flex-start'} justifyContent={'space-around'} flexDirection={'row'} spacing={3}>
                 {data.Modem.map((item) => (
-                    <Box key={item.name}>
+                    <Box key={item.name} borderWidth={2} p={10} m={5}>
                         <Heading size={'lg'}>
                             {item.name}
                         </Heading>
@@ -45,19 +51,44 @@ export const ModemList = () => {
                         <Text>
                             {item.desc_features}
                         </Text>
-                        <Button as={'a'} href='/register'>Get Started</Button>
+                        <Button 
+                            as={'a'} 
+                            href='/register' w={'100%'} 
+                            bgColor={'rgb(44, 75, 255)'} 
+                            color={'white'}
+                            _hover={{bgColor: 'blue'}}
+                        >
+                            Get Started
+                        </Button>
                         <Box>
                             {
                                 item.features.map((feature) => (
                                     <Box key={feature}>
-                                        <Text>{feature}</Text>
+                                        <HStack>
+                                            <Text as={'span'}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" 
+                                                    width="24" 
+                                                    height="24" 
+                                                    viewBox="0 0 24 24"
+                                                    fill="black"
+                                                    >
+                                                        <path d="M22 2v20h-20v-20h20zm2-2h-24v24h24v-24zm-5.541 
+                                                                8.409l-1.422-1.409-7.021 7.183-3.08-2.937-1.395 
+                                                                1.435 4.5 4.319 8.418-8.591z"/>
+                                                </svg>
+                                            </Text>
+
+                                            <Text>
+                                                {feature}
+                                            </Text>
+                                        </HStack>
                                     </Box>
                                 ))
                             }
                         </Box>
                     </Box>
                 ))}
-            </Flex>
+            </HStack>
         </Box>
     )
 }
