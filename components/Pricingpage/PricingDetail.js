d120ddda7de4c9db93af1f7bac3bff7aac0977c5
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-10 12:08:04 +0100

    Added pricing page main component

---
+++ components/Pricingpage/PricingDetail.js
@@ -0,0 +1,11 @@
+import PricingBanner from "./Banner"
+import PricingList from "./List"
+
+export default function PricingDetail() {
+    return(
+        <>
+            <PricingBanner />
+            <PricingList />
+        </>
+    )
+}
