123b5060c91dab238fd46f2493b401095ff7f545
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-13 14:19:09 +0100

    Modified mobile & desktop view

--- components/Homepage/Banner.js
+++ components/Homepage/Banner.js
@@ -13,18 +13,19 @@ export default function Banner(){
     return(
       <Box mt={"2em"}>
             <Flex 
-                alignItems={'flex-start'} 
+                alignItems={{base: 'center', md: 'center', lg: 'flex-start'} }
                 justifyContent={'space-around'} 
                 direction={{
                   base: 'column', 
-                  md: 'row',
+                  md: 'column',
                   lg: 'row'}}
                   mt={10}
                   fontFamily={"'Poppins', sans-serif"} 
                 >
               <Box flex={5}>
                 <Heading
-                size={{base: "sm", md: "md", lg: "lg"}}
+                fontSize={{base: "1em", md: "2em", lg: "2.5em"}}
+                textAlign={{base: 'center', md: 'center', lg: 'left'}}
                 mb={10}
                 >
                   FAST AND SECURED NETWORK TO YOUR DOORSTEP
@@ -33,12 +34,12 @@ export default function Banner(){
                   mb={7}
                   fontWeight={300}
                   textAlign={{base: 'center', md: 'center', lg: 'left'}}
-                  size={{base: "sm", md: "md", lg: "lg"}}
+                  fontSize={{base: "sm", md: "md", lg: "lg"}}
                 >
                   Wifispace is one of the best network service provider in whole of nigeria.  We&apos;re a values-driven company that provides affordable, intuitive, networking tools for ambitious people and teams.</Text>
                 <HStack spacing={5} rowGap={5} justifyContent={{base: 'center', md: 'center', lg: 'flex-start'}}>
-                  <Button as={'a'} href={'/register'} fontWeight={'200'} bgColor={'blue.600'} color={'white'} _hover={{bgColor: 'blue.800'}}>Get Started</Button>
-                  <Button as={'a'} href={'#'} bgColor={''} fontWeight={'200'} border={'1px solid blue'} color={'blue'}>Learn More</Button>
+                  <Button as={'a'} href={'/register'} w={{base: '100%', md: '100%', lg: 'inherit'}} fontWeight={'200'} bgColor={'blue.600'} color={'white'} _hover={{bgColor: 'blue.800'}}>Get Started</Button>
+                  <Button as={'a'} href={'#'} bgColor={''} fontWeight={'200'} w={{base: '100%', md: '100%', lg: 'inherit'}} border={'1px solid blue'} color={'blue'}>Learn More</Button>
               </HStack>
               </Box>
                 <Image 
