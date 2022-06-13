782cb2e23ef143038130c5df91b63c6147ec8926
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-13 12:26:12 +0100

    Modified features styling

--- components/Homepage/Features.js
+++ components/Homepage/Features.js
@@ -6,28 +6,87 @@ import {
     Image, 
     Flex, 
     Button, 
-    HStack } from '@chakra-ui/react'
+    HStack,
+    Spacer
+ } from '@chakra-ui/react'
 import data from '../../lib/data.json'
 
 export default function Features(){
     return(
-        <Stack>
-            <Box>
+        <>
+            <Box marginX={'2em'}>
                 {
                     data.Features.map((features, index) => (
-                        <Box key={index}>
-                            <Flex justifyContent={'space-around'}>
-                                <Box>
-                                    <Heading>{features.heading}</Heading>
-                                    <Text>{features.description}</Text>
-                                    <HStack mt={2}>
-                                        <Button as={'a'} href={'/register'}>Get started</Button>
-                                        <Button as={'a'} href={'/services'}>Learn More</Button>
+                            <Flex 
+                                key={features.description}
+                                justifyContent={'space-around'}
+                                flexDirection={{base: 'column', md: 'row', lg: 'row'}}
+                                marginX={20}
+                                marginY={20}
+                                _even={{
+                                    sm: {flexDirection: 'row-reverse', flexWrap: 'wrap'},
+                                    md: {flexWrap: 'nowrap', flexDirection: 'column'}, 
+                                    lg: {flexWrap: 'nowrap', flexDirection: 'column'}
+                                }}
+                                 >
+                                <Box marginX={{base: 5, md: 7, lg: 10}}>
+                                    <HStack >
+                                        <Heading
+                                            fontFamily={"'Poppins', sans-serif"}
+                                            color={'rgb(44, 75, 255)'}
+                                            fontSize={{base: '0.8em', md: '1.2em', lg: '1.6em'}}>
+                                            {++index}.
+                                        </Heading>
+                                        <Heading 
+                                            fontFamily={"'Poppins', sans-serif"}
+                                            color={'rgb(44, 75, 255)'}
+                                            fontSize={{base: '0.8em', md: '1.2em', lg: '1.6em'}}>
+                                            {features.heading}
+                                        </Heading>
                                     </HStack>
+                                        <Text 
+                                            fontFamily={"'Poppins', sans-serif"}
+                                            mt={5}
+                                            fontSize={{base: '0.7em', md: '0.8em', lg: '1em'}}>
+                                            {features.description}
+                                        </Text>
+                                    <Flex
+                                        mt={10}
+                                        flexDirection={{base: 'column', md: 'row', lg: 'row'}}
+                                        >
+                                        <Button 
+                                            as={'a'} 
+                                            href={'/register'} 
+                                            bgColor={'rgb(44, 75, 255)'} 
+                                            color={'white'} 
+                                            _hover={{bgColor: 'blue'}}
+                                            fontFamily={"'Poppins', sans-serif"}
+                                            letterSpacing={1}
+                                            mx={{base: 0, md: 5, lg: 5}}
+                                            fontSize={{base: '0.7em', md: '0.8em', lg: '1em'}}
+                                            w={{base: '100%', md: 'auto', lg: 'auto'}}
+                                            mb={5}
+                                            >
+                                                Get started now
+                                        </Button>
+                                        <Button 
+                                            as={'a'} 
+                                            href={'/services'} 
+                                            border={'1px solid rgb(44, 75, 255)'} 
+                                            color={'rgb(44, 75, 255)'}
+                                            fontFamily={"'Poppins', sans-serif"}
+                                            fontSize={{base: '0.7em', md: '0.8em', lg: '1em'}}
+                                            >
+                                                Learn More
+                                            </Button>
+                                    </Flex>
                                 </Box>
-                                <Image src={features.image} alt={'features'} objectFit={'cover'} boxSize={'md'} />
+                                <Image 
+                                    src={features.image} 
+                                    alt={'features'} 
+                                    objectFit={'cover'} 
+                                    boxSize={{base: 'md', md: 'md', lg: 'lg'}} />
                             </Flex>
-                        </Box>
                     ))
                 }
             </Box>
@@ -44,6 +103,6 @@ export default function Features(){
                             ))
                         }
                     </HStack>
-        </Stack>
+        </>
     )
 }
