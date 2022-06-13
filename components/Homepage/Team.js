94de5127cc5fef540aab73d34a7e697e0a12ecf6
Kiddo-prog <husseinmubarak500@gmail.com>
2022-06-13 13:13:23 +0100

    Updated team section

--- components/Homepage/Team.js
+++ components/Homepage/Team.js
@@ -1,17 +1,54 @@
 import { Heading } from "@chakra-ui/react"
-import { Stack, Flex, Box } from "@chakra-ui/react"
+import { Flex, Box, Image, Text, Button, Spacer } from "@chakra-ui/react"
+import data from '../../lib/data.json'
 
 export default function Team() {
     return(
-        <div>
-            <Heading>Team</Heading>
-            <Stack>
-                <Flex>
-                    <Box as={'div'}>
-                    
+            <Flex 
+                marginTop={'10em'}
+                flexDirection={{base: 'column-reverse', md: 'row', lg: 'row'}}
+                alignItems={'center'}
+                textAlign={{base: 'center', md: 'left', lg: 'left'}}
+            >
+                    <Box flex={5}>
+                    <Image
+                        flex={5} 
+                        src={'/images/wifibase-team.jpg'} 
+                        alt={'our-team'} />
+                    </Box>
+                    <Spacer />
+                    <Box 
+                        flex={5}
+                        mb={10}>
+                        <Heading
+                            fontFamily={"'Poppins', sans-serif"}
+                            as={'h3'}
+                            size={'lg'}
+                            fontWeight={800}
+                            mb={10}
+                        >
+                            And we are here to help
+                        </Heading>
+                        <Text
+                            fontSize={{base: '1em', md: '0.7em', lg: '0.9em'}}
+                            fontFamily={"'Poppins', sans-serif"}
+                            lineHeight={{base: 2, md: 8, lg: 8}}
+                        >
+                            {data.CustomerDescription}
+                        </Text>  
+                        <Button
+                            border={'1px solid rgb(44, 75, 255)'} 
+                            color={'rgb(44, 75, 255)'}
+                            fontFamily={"'Poppins', sans-serif"}
+                            mt={10}
+                            backgroundColor={'white'}
+                            fontWeight={300}
+                            size={{base: 'md', md: 'lg', lg: 'lg'}}
+                            textAlign={'center'}
+                        >
+                            Visit our help center
+                        </Button>
                     </Box>
                 </Flex>
-            </Stack>
-        </div>
     )
 }
